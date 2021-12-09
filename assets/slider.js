34fea212c183a2e8f3397f0f35967ecf33c2f002
AncleMarvel <anlcemarvel@gmail.com>
2021-12-09 12:33:23 +0200

    Deleted script from theme

--- assets/slider.js
+++
@@ -1,6 +0,0 @@
-function initSlider(sliderSelector, slidesSelector) {
-    const slider = $(sliderSelector);
-    const slides = $(slidesSelector).each(() => {console.log});
-}
-
-initSlider("#collection-slider__karhu", ".collection-slider__item");
